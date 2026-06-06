86676f1717b8a588ab8f987a040bd3bafd4c5556
unknown <sanskar>
2026-06-06 16:56:37 +0530

    uploaded day 6 assignment

---
+++ day6/q24.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long num;
+    cin >> num;
+
+    long long largestPrimeFactor = -1;
+
+    while (num % 2 == 0) {
+        largestPrimeFactor = 2;
+        num /= 2;
+    }
+
+    for (long long i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            largestPrimeFactor = i;
+            num /= i;
+        }
+    }
+
+    if (num > 2)
+        largestPrimeFactor = num;
+
+    cout << "Largest Prime Factor = " << largestPrimeFactor;
+
+    return 0;
+}
