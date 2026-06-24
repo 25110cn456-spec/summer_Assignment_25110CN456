2446ec6850bec8ae22eec08db2800b3ac28ccdf6
unknown <sanskar>
2026-06-24 16:07:21 +0530

    uploaded day 24 assignment

---
+++ day24/q96.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    string result = "";
+
+    for (int i = 0; i < str.length(); i++) {
+        bool isDuplicate = false;
+
+        for (int j = 0; j < result.length(); j++) {
+            if (str[i] == result[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        if (!isDuplicate) {
+            result += str[i];
+        }
+    }
+
+    cout << "String after removing duplicates: " << result;
+
+    return 0;
+}
